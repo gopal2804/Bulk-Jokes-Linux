f6ad32af51064bf429fa920119c7dca7981c61c3
GOPAL GUPTA <gopalgupta2804@gmail.com>
2022-07-25 12:09:24 +0530

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/bash
-var="abcdefghijklmnopqrstuvwxyz" #dfdfdf
-count=1
-while (( $count < 500 ))
+var="abcdefghijklmnopqrstuvwxyz" #using var to strore all the english aplhabets 
+count=1 #to count the number of mails sent
+while (( $count < 500 )) #settinng the limit
 do
-    var1=${var:$(( RANDOM % ${#var} )):1}
-    var2=${var:$(( RANDOM % ${#var} )):1}
-    if [ -e jok$var1$var2 ]
+    var1=${var:$(( RANDOM % ${#var} )):1}  #aceesing the random variable from var
+    var2=${var:$(( RANDOM % ${#var} )):1} 
+    if [ -e jok$var1$var2 ] #checking whether the file exits or not in the main directory after concatinating var1 and var2.
     then
         cat jok$var1$var2 | mailx --append='FROM:Your name' --append='BCC:bcc-recepient1@gmail.com,bcc-recipient2@gmail.com' -s "Joke of the day $count" -- 
-        main_recipient@gmail.com
+        main_recipient@gmail.com #syntax for sending mail through mailx
         (( count++ ))
     else
         continue
